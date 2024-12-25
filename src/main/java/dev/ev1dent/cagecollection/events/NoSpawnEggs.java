039da57eb7ea522bd9f2606e8c96ea32d01b946d
Evidentsinger14 <cdelancey186423@gmail.com>
2024-12-25 09:46:50 -0500

    reformat & implement method for admin to modify spawner with egg

--- src/main/java/dev/ev1dent/cagecollection/events/NoSpawnEggs.java
+++ src/main/java/dev/ev1dent/cagecollection/events/NoSpawnEggs.java
@@ -1,6 +1,7 @@
 package dev.ev1dent.cagecollection.events;
 
 import com.destroystokyo.paper.MaterialTags;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,11 +13,20 @@ public class NoSpawnEggs implements Listener {
     @EventHandler
     public void onEggUsage(PlayerInteractEvent event){
         Player player = event.getPlayer();
+
         if(event.getClickedBlock() == null) return;
+
         if(!event.getClickedBlock().getType().equals(Material.SPAWNER)) return;
+
         if(MaterialTags.SPAWN_EGGS.isTagged(player.getInventory().getItemInOffHand())
                 || MaterialTags.SPAWN_EGGS.isTagged(player.getInventory().getItemInMainHand())){
+            if(isAdmin(player)) return;
             event.setCancelled(true);
         }
+    }
+    private boolean isAdmin(Player player){
+        return player.isSneaking()
+                && player.hasPermission("cagecollection.break.override")
+                && player.getGameMode() == GameMode.CREATIVE;
     }
 }
