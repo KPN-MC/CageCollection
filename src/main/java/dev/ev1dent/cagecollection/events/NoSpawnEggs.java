b63d8be7299ca132c434fd2d077ff429e3661186
Evidentsinger14 <cdelancey186423@gmail.com>
2024-12-25 20:45:54 -0500

    make less ugly

--- src/main/java/dev/ev1dent/cagecollection/events/NoSpawnEggs.java
+++ src/main/java/dev/ev1dent/cagecollection/events/NoSpawnEggs.java
@@ -7,26 +7,29 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.jetbrains.annotations.NotNull;
 
 public class NoSpawnEggs implements Listener {
 
     @EventHandler
-    public void onEggUsage(PlayerInteractEvent event){
+    public void onEggUsage(@NotNull PlayerInteractEvent event) {
         Player player = event.getPlayer();
 
-        if(event.getClickedBlock() == null) return;
+        if (event.getClickedBlock() == null || !event.getClickedBlock().getType().equals(Material.SPAWNER)) return;
 
-        if(!event.getClickedBlock().getType().equals(Material.SPAWNER)) return;
-
-        if(MaterialTags.SPAWN_EGGS.isTagged(player.getInventory().getItemInOffHand())
-                || MaterialTags.SPAWN_EGGS.isTagged(player.getInventory().getItemInMainHand())){
-            if(isAdmin(player)) return;
+        if (isHoldingSpawnEgg(player) && !isAdmin(player)) {
             event.setCancelled(true);
         }
     }
-    private boolean isAdmin(Player player){
-        return player.isSneaking()
-                && player.hasPermission("cagecollection.break.override")
-                && player.getGameMode() == GameMode.CREATIVE;
+
+    private boolean isAdmin(@NotNull Player player) {
+        return player.isSneaking() &&
+                player.hasPermission("cagecollection.break.override") &&
+                player.getGameMode() == GameMode.CREATIVE;
+    }
+
+    private boolean isHoldingSpawnEgg(@NotNull Player player) {
+        return MaterialTags.SPAWN_EGGS.isTagged(player.getInventory().getItemInMainHand()) ||
+                MaterialTags.SPAWN_EGGS.isTagged(player.getInventory().getItemInOffHand());
     }
 }
