cbe545617bf353c6c2dafffd0217e20008742a71
Evidentsinger14 <cdelancey186423@gmail.com>
2024-12-25 20:48:34 -0500

    add spawner placed message

--- src/main/java/dev/ev1dent/cagecollection/events/SpawnerPlaceEvent.java
+++ src/main/java/dev/ev1dent/cagecollection/events/SpawnerPlaceEvent.java
@@ -1,6 +1,7 @@
 package dev.ev1dent.cagecollection.events;
 
 import dev.ev1dent.cagecollection.CCMain;
+import dev.ev1dent.cagecollection.utilities.Utils;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.CreatureSpawner;
@@ -13,6 +14,8 @@ import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 
 public class SpawnerPlaceEvent implements Listener {
+
+    private final Utils Utils = new Utils();
 
     CCMain plugin;
     public SpawnerPlaceEvent(CCMain plugin){
@@ -36,6 +39,7 @@ public class SpawnerPlaceEvent implements Listener {
             spawner.setSpawnedType(EntityType.PIG);
         }
         spawner.update();
+        event.getPlayer().sendMessage(Utils.formatMM(mobType + "Spawner placed."));
 
     }
 }
